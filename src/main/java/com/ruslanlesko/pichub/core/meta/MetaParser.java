3b1cb9e12d3b1b88126cebdee00b45e328a0b059
Ruslan Lekso <ruslanlesko@gmail.com>
2020-02-21 16:57:58 +0200

    Rotate image if needed on insertion step

--- src/main/java/com/ruslanlesko/pichub/core/meta/MetaParser.java
+++ src/main/java/com/ruslanlesko/pichub/core/meta/MetaParser.java
@@ -3,6 +3,9 @@ package com.ruslanlesko.pichub.core.meta;
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifIFD0Directory;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,21 +18,53 @@ import java.util.Date;
 
 public class MetaParser {
     private static Logger logger = LoggerFactory.getLogger("Application");
-    private final byte[] data;
+
+    private Metadata metadata;
 
     public MetaParser(byte[] data) {
-        this.data = data;
+        try {
+            this.metadata = ImageMetadataReader.readMetadata(new ByteArrayInputStream(data));
+        } catch (ImageProcessingException | IOException e) {
+            logger.error(e.getMessage());
+            throw new IllegalArgumentException(e);
+        }
     }
 
     public LocalDateTime getDateCaptured() {
+        ExifSubIFDDirectory exifDirectory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+        Date date = exifDirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    public int getRotation() {
         try {
-            Metadata metadata = ImageMetadataReader.readMetadata(new ByteArrayInputStream(data));
-            ExifSubIFDDirectory exifDirectory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-            Date date = exifDirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-            return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-        } catch (ImageProcessingException | IOException e) {
-            logger.error("Cannot extract capturing date: " + e.getMessage());
-            return null;
+            ExifIFD0Directory exifDirectory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+            int orientation = exifDirectory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+            int degrees;
+            switch (orientation) {
+                case 3: {
+                    degrees = 180;
+                    break;
+                }
+                case 6: {
+                    degrees = 90;
+                    break;
+                }
+                case 8: {
+                    degrees = 270;
+                    break;
+                }
+                default: {
+                    degrees = 0;
+                    break;
+                }
+            }
+            logger.info("Orientation: " + orientation);
+            logger.info("Rotating image to " + degrees + " degrees");
+            return degrees;
+        } catch (MetadataException e) {
+            logger.error(e.getMessage());
+            return -1;
         }
     }
 }
--- src/main/java/com/ruslanlesko/pichub/core/services/impl/PictureServiceImpl.java
+++ src/main/java/com/ruslanlesko/pichub/core/services/impl/PictureServiceImpl.java
@@ -10,6 +10,13 @@ import com.ruslanlesko.pichub.core.services.PictureService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -81,11 +88,53 @@ public class PictureServiceImpl implements PictureService {
 
         MetaParser metaParser = new MetaParser(data);
         LocalDateTime dateCaptured = metaParser.getDateCaptured();
+        int degrees = metaParser.getRotation();
+
+        data = rotateImage(data, degrees);
 
         String path = pictureDataDao.save(data);
         PictureMeta meta = new PictureMeta(-1, userId, albumId.orElseGet(() -> -1L), path, LocalDateTime.now(), dateCaptured);
         long id = pictureMetaDao.save(meta);
         logger.info("Inserted new picture with id {} for user id {}", id, userId);
         return id > 0 ? Optional.of(id) : Optional.empty();
+    }
+
+    private byte[] rotateImage(byte[] bytes, int degrees) {
+        if (degrees <= 0) {
+            return bytes;
+        }
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            BufferedImage image = ImageIO.read(bais);
+
+            final double rads = Math.toRadians(degrees);
+            final double sin = Math.abs(Math.sin(rads));
+            final double cos = Math.abs(Math.cos(rads));
+            final int w = (int) Math.floor(image.getWidth() * cos + image.getHeight() * sin);
+            final int h = (int) Math.floor(image.getHeight() * cos + image.getWidth() * sin);
+            final BufferedImage rotatedImage = new BufferedImage(w, h, image.getType());
+            final AffineTransform at = new AffineTransform();
+            at.translate(w / 2, h / 2);
+            at.rotate(rads,0, 0);
+            at.translate(-image.getWidth() / 2, -image.getHeight() / 2);
+            final AffineTransformOp rotateOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BICUBIC);
+            rotateOp.filter(image, rotatedImage);
+
+            ImageIO.write(rotatedImage, "JPEG", baos);
+            baos.flush();
+            byte[] resultBytes = baos.toByteArray();
+            return resultBytes;
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            return null;
+        } finally {
+            try {
+                bais.close();
+                baos.close();
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
+        }
     }
 }
