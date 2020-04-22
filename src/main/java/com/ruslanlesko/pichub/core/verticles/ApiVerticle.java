09e1c2a9580704d4cba97214d27dc1e4234a44f6
Ruslan Lekso <ruslanlesko@gmail.com>
2020-04-22 18:55:43 +0300

    Fix cors one more time

--- src/main/java/com/ruslanlesko/pichub/core/verticles/ApiVerticle.java
+++ src/main/java/com/ruslanlesko/pichub/core/verticles/ApiVerticle.java
@@ -47,7 +47,7 @@ public class ApiVerticle extends AbstractVerticle {
         router.options().handler(r ->r.response()
                 .putHeader("Access-Control-Allow-Headers", "content-type, authorization")
                 .putHeader("Access-Control-Allow-Origin", "*")
-                .putHeader("Access-Control-Allow-Methods", "GET, DELETE, PUT, POST, OPTIONS")
+                .putHeader("Access-Control-Allow-Methods", "GET, DELETE, PATCH, POST, OPTIONS")
                 .putHeader("Access-Control-Max-Age", "-1")
                 .end()
         );
