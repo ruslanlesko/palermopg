c1b01cdbcab96135051e8b6d952462aa6a595e3c
Ruslan Lekso <ruslanlesko@gmail.com>
2020-06-17 15:28:58 +0300

    Refactor to use Futures properly

--- src/main/java/com/ruslanlesko/pichub/core/util/ReactiveSubscriber.java
+++ src/main/java/com/ruslanlesko/pichub/core/util/ReactiveSubscriber.java
@@ -7,29 +7,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class ReactiveSubscriber<T, R> implements Subscriber<T> {
     private static final Logger logger = LoggerFactory.getLogger("Application");
 
     private final Promise<R> promise;
     private final Function<T, R> onResult;
+    private final Predicate<T> successCriteria;
+    private final boolean isVoid;
     private final R defaultValue;
+    private final Throwable onFail;
 
     private Throwable error = null;
     private R result = null;
+    private boolean succeeded;
 
     public static <T, R> ReactiveSubscriber<T, R> forPromise(Promise<R> promise, Function<T, R> onResult) {
-        return new ReactiveSubscriber<>(promise, onResult, null);
+        return new ReactiveSubscriber<>(promise, onResult, null, false, null, null);
     }
 
     public static <T, R> ReactiveSubscriber<T, R> forPromise(Promise<R> promise, Function<T, R> onResult, R defaultValue) {
-        return new ReactiveSubscriber<>(promise, onResult, defaultValue);
+        return new ReactiveSubscriber<>(promise, onResult, null, false, defaultValue, null);
     }
 
-    protected ReactiveSubscriber(Promise<R> promise, Function<T, R> onResult, R defaultValue) {
+    public static <T, R> ReactiveSubscriber<T, R> forVoidPromise(Promise<R> promise, Predicate<T> successCriteria, Throwable onFail) {
+        return new ReactiveSubscriber<>(promise, null, successCriteria, true, null, onFail);
+    }
+
+    protected ReactiveSubscriber(
+            Promise<R> promise,
+            Function<T, R> onResult,
+            Predicate<T> successCriteria,
+            boolean isVoid, R defaultValue,
+            Throwable onFail
+    ) {
         this.promise = promise;
         this.onResult = onResult;
+        this.successCriteria = successCriteria;
+        this.isVoid = isVoid;
         this.defaultValue = defaultValue;
+        this.onFail = onFail;
     }
 
     @Override
@@ -40,6 +58,10 @@ public class ReactiveSubscriber<T, R> implements Subscriber<T> {
     @Override
     public void onNext(T next) {
         logger.debug("Next arrived");
+        if (isVoid) {
+            succeeded = successCriteria.test(next);
+            return;
+        }
         result = onResult.apply(next);
     }
 
@@ -54,6 +76,14 @@ public class ReactiveSubscriber<T, R> implements Subscriber<T> {
     public void onComplete() {
         logger.debug("Completing...");
         if (error == null) {
+            if (isVoid) {
+                if (succeeded) {
+                    promise.complete();
+                } else {
+                    promise.fail(onFail);
+                }
+                return;
+            }
             promise.complete(result == null ? defaultValue : result);
         }
     }
