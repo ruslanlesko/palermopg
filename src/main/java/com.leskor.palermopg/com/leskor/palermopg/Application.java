e88eacb6cabcdca8c7fff31ad3ca682b189dc356
Ruslan Lesko <ruslanlesko@gmail.com>
2021-03-15 21:04:22 +0200

    Bump up version

--- src/main/java/com.leskor.palermopg/com/leskor/palermopg/Application.java
+++ src/main/java/com.leskor.palermopg/com/leskor/palermopg/Application.java
@@ -59,7 +59,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        logger.info("Starting PalermoPG 1.15");
+        logger.info("Starting PalermoPG 1.15.1");
         Application palermoPG = new Application();
         palermoPG.startHttpServer();
     }
