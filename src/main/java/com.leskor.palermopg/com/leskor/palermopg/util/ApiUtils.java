160f0053c5a29f6857e0372b821ae7b60d6adf80
Ruslan Lesko <ruslanlesko@gmail.com>
2021-03-21 19:16:37 +0200

    Add logging for exceptions

--- src/main/java/com.leskor.palermopg/com/leskor/palermopg/util/ApiUtils.java
+++ src/main/java/com.leskor.palermopg/com/leskor/palermopg/util/ApiUtils.java
@@ -4,8 +4,12 @@ import com.leskor.palermopg.exception.AuthorizationException;
 import com.leskor.palermopg.exception.MissingItemException;
 import com.leskor.palermopg.exception.StorageLimitException;
 import io.vertx.core.http.HttpServerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApiUtils {
+    private static final Logger logger = LoggerFactory.getLogger("Application");
+
     public static void handleFailure(Throwable cause, HttpServerResponse response) {
         if (cause instanceof AuthorizationException) {
             cors(response.setStatusCode(401)).end();
@@ -19,6 +23,7 @@ public class ApiUtils {
             cors(response.setStatusCode(400)).end(((StorageLimitException) cause).json());
             return;
         }
+        logger.error("Exception occurred", cause);
         cors(response.setStatusCode(500)).end();
     }
 
