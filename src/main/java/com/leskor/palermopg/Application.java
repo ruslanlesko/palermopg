71bf05b2aa5e08a23966204adeec72ca0a69b3bf
Ruslan Lesko <ruslanlesko@gmail.com>
2021-12-11 21:27:57 +0200

    Upgrade log4j library to 2.15.0

--- src/main/java/com/leskor/palermopg/Application.java
+++ src/main/java/com/leskor/palermopg/Application.java
@@ -95,7 +95,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        logger.info("Starting PalermoPG 1.21.2");
+        logger.info("Starting PalermoPG 1.21.3");
         Application palermoPG = new Application();
         palermoPG.startHttpServer();
     }
